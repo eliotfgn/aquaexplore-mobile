3d76350e3554e444a23c6a3a4cc9ed6338f40ea1
Eliot Fagnon <efagnon2002@gmail.com>
2024-11-19 07:41:38 +0100

    style: Remove boilerplate code

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import { Stack } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Home() {
+  return (
+    <>
+      <Stack.Screen options={{ title: "Home" }} />
+      <View>
+        <Text>Hello world</Text>
+      </View>
+    </>
+  );
+}
+
